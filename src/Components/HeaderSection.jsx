aa7b99743937721269eb3a499f64ea8314578612
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-06-08 22:41:52 +0530

    Header section commit

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -4,43 +4,20 @@ const HeaderSection = () => {
   return (
     <div>
       <h1>HeaderSection</h1>
+      {/* NAVBAR SECTION */}
       <header>
-        <a href="index.html" class="logo navbar-brand">
-          Vallarasu k
-        </a>
-        <div class="bx bx-menu" id="menu-icons"></div>
-
-        <ul class="navbar" id="navbar">
-          <li>
-            <a class="text-decoration-none nav-item" href="#home">
-              HOME
-            </a>
-          </li>
-          <li>
-            <a class="text-decoration-none nav-item" href="#about">
-              ABOUT
-            </a>
-          </li>
-          <li>
-            <a class="text-decoration-none nav-item" href="#skills">
-              SKILLS
-            </a>
-          </li>
-          <li>
-            <a class="text-decoration-none nav-item" href="#services">
-              SERVICES
-            </a>
-          </li>
-          <li>
-            <a class="text-decoration-none nav-item" href="#portfolio">
-              PORTFOLIO
-            </a>
-          </li>
-          <li>
-            <a class="text-decoration-none nav-item" href="#contact">
-              CONTACT
-            </a>
-          </li>
+        {/* Logo */}
+        <a href="index.html" className="logo navbar-brand">Vallarasu k</a>
+        <div className="bx bx-menu" id="menu-icons"></div>
+      
+        {/* Menu items */}
+        <ul className="navbar" id="navbar">
+          <li><a className="text-decoration-none nav-item" href="#home">HOME</a></li>
+          <li><a className="text-decoration-none nav-item" href="#about">ABOUT</a></li>
+          <li><a className="text-decoration-none nav-item" href="#skills">SKILLS</a></li>
+          <li><a className="text-decoration-none nav-item" href="#services">SERVICES</a></li>
+          <li><a className="text-decoration-none nav-item" href="#portfolio">PORTFOLIO</a></li>
+          <li><a className="text-decoration-none nav-item" href="#contact">CONTACT</a></li>
         </ul>
       </header>
     </div>
